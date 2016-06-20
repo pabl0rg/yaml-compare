851d8d975c80a4f9afeab0c2e3c25696fd58ea41
Juan Liska <jpliska@gmail.com>
2016-06-19 22:02:11 -0600

    simplify and improve output

--- src/main/kotlin/com/guatec/yaml/YamlCompare.kt
+++ src/main/kotlin/com/guatec/yaml/YamlCompare.kt
@@ -41,7 +41,7 @@ object YamlCompare {
         return null;
     }
 
-    fun compare(files: List<String>, groupByKey:String, rawCheckKeys:List<String>, verbose: Boolean=false) {
+    fun compare(files: List<String>, groupByKey:String, rawKeys:List<String>, verbose: Boolean=false) {
         val yamls = files.map {
             if (verbose)
                 println("loading: $it")
@@ -54,36 +54,39 @@ object YamlCompare {
         }
 
         //convert wildcard checkKeys by resolving available keys
-        val checkKeys: List<String?> = rawCheckKeys.flatMap { rawPrintKey ->
+        val realKeys: List<String> = rawKeys.flatMap { rawPrintKey ->
             var resolved: Any? = null
             if (rawPrintKey.endsWith(".*"))
                 resolved = yamls.first().getNested(rawPrintKey)
 
             when (resolved) {
                 is Iterable<*> -> resolved.map { leaf ->
-                    rawPrintKey.replace(Regex("\\*$"), leaf as String)}
+                    rawPrintKey.replace(Regex("\\*$"), leaf as String)
+                }
                 else -> listOf(rawPrintKey)
             }
-        }
+        }.filterNotNull()
 
-        checkKeys.filterNotNull().forEach { keyToCheck ->
-            grouped.keys.sortedBy { it.toString() }.forEach { groupKey ->
-                val group = grouped.get(groupKey)!!
+        grouped.keys.sortedBy { it.toString() }.forEach { groupKey ->
+            val group = grouped.get(groupKey)!!
+            println("Group: ${groupKey}")
 
-                if (verbose) {
-                    println("\t${groupKey}")
-                    group.forEach { group ->
-                        val name = group["__yaml_file__"]
-                        println("\t\t$name: ${group.getNested(keyToCheck)}")
+            if (verbose) {
+                realKeys.forEach { keyToPrint ->
+                    println("\t$keyToPrint")
+                    group.forEach { yaml ->
+                        val name = yaml["__yaml_file__"]
+                        println("\t\t$name: ${yaml.getNested(keyToPrint)}")
                     }
-                } else {
+                }
+            } else {
+                realKeys.forEach { keyToCheck ->
                     val groupsWithCollision = group.groupBy { it.getNested(keyToCheck) }
                             .filterValues { instancesInGroup -> instancesInGroup.size > 1 }
 
                     groupsWithCollision.entries.forEach { groupWithCollision ->
                         if (groupWithCollision.key != null) {
-                            println("group:${groupKey}")
-                            println("\tCOLLISION $keyToCheck: ${groupWithCollision.key}")
+                            println("\tCollision @ $keyToCheck: ${groupWithCollision.key}")
                             groupWithCollision.value.forEach {
                                 val name = it["__yaml_file__"]
                                 println("\t\t$name")
